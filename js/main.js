691fc6e34a2ac09dc2b2b7bc0a56077dc62a9599
Anh <demonxranger8@gmail.com>
2020-11-11 18:22:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-const scene = new THREE.scene();
+const scene = new THREE.Scene(),
+      canvas = document.getElementById('canvas'),
+      sWIDTH = canvas.width, sHEIGHT = canvas.height,
+      camera = new THREE.PerspectiveCamera( 75, sWIDTH / sHEIGHT, 0.1, 1000 ),
+      renderer = new THREE.WebGLRenderer({ canvas: canvas});
+renderer.setSize( window.innerWidth, window.innerHeight );
