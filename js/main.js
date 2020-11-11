51500e76578f3ff8b93a8bf1fe22b8490457c5b3
Anh <demonxranger8@gmail.com>
2020-11-11 21:05:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,25 @@ const scene = new THREE.Scene(),
       canvas = document.getElementById('canvas'),
       sWIDTH = canvas.width, sHEIGHT = canvas.height,
       camera = new THREE.PerspectiveCamera( 75, sWIDTH / sHEIGHT, 0.1, 1000 ),
-      renderer = new THREE.WebGLRenderer({ canvas: canvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer = new THREE.WebGLRenderer({canvas: canvas});
+
+renderer.setSize(sWIDTH, sHEIGHT);
+
+const testPlaneGeo = new THREE.PlaneGeometry(5, 5),
+      testMat = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide} ),
+      testPlane = new THREE.Mesh(testPlaneGeo, testMat);
+
+scene.add(testPlane);
+camera.position.z = 5;
+camera.position.x = 2;
+testPlane.position.y = -1;
+
+function animateScene() {
+    requestAnimationFrame(animateScene );
+    renderer.render(scene, camera);
+   
+}
+animateScene();
+
+
+var controls = new THREE.TrackballControls(camera, renderer.domElement);
