f0e4574feba78c81d800ff025e776219f6fb4005
Anh <demonxranger8@gmail.com>
2020-11-24 11:21:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const scene = new THREE.Scene(),
       canvas = document.getElementById('canvas'),
       sWIDTH = canvas.width, sHEIGHT = canvas.height,
-      camera = new THREE.PerspectiveCamera( 75, sWIDTH / sHEIGHT, 0.1, 1000 ),
+      camera = new THREE.PerspectiveCamera( 75, sWIDTH / sHEIGHT, 0.1, 10000 ),
       renderer = new THREE.WebGLRenderer({canvas: canvas});
 
 renderer.setSize(sWIDTH, sHEIGHT);
@@ -15,12 +15,18 @@ camera.position.z = 5;
 camera.position.x = 2;
 testPlane.position.y = -1;
 
+var controls = new THREE.TrackballControls(camera, renderer.domElement);
+controls.rotateSpeed = 2.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+
+controls.keys = [ 65, 83, 68 ];
+camera.position.set( 0, 20, 100 );
+controls.update();
+
 function animateScene() {
     requestAnimationFrame(animateScene );
     renderer.render(scene, camera);
-   
+    controls.update();
 }
 animateScene();
-
-
-var controls = new THREE.TrackballControls(camera, renderer.domElement);
